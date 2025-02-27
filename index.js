fff8afe45b5ee18096de8aa665ab04f44ccf39be
marcelohoficial <marcelo.12.musico@gmail.com>
2025-02-27 10:43:09 -0300

    recovery

---
+++ index.js
@@ -0,0 +1,23 @@
+const xlsx = require("xlsx");
+const path = require("path");
+
+// Caminho do arquivo Excel
+const filePath = path.join(__dirname + "/upload", "list.xlsx");
+
+// Ler o arquivo
+const workbook = xlsx.readFile(filePath);
+
+// Selecionar a primeira planilha
+const sheetName = workbook.SheetNames[0];
+const sheet = workbook.Sheets[sheetName];
+
+// Converter a planilha em JSON
+const data = xlsx.utils.sheet_to_json(sheet, { header: 1 });
+
+// Pegar todos os IDs da primeira coluna (ignorando o cabeçalho)
+const ids = data
+	.slice(1)
+	.map((row) => row[0])
+	.filter((id) => id !== undefined && id.length === 32);
+
+console.log("Lista de IDs:", ids);
---
+++ src/start.js
@@ -0,0 +1,51 @@
+const puppeteer = require("puppeteer");
+const xlsx = require("xlsx");
+const path = require("path");
+
+async function start(ids) {
+	// Iniciar o navegador Puppeteer
+	const chromePath =
+		"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+
+	const browser = await puppeteer.launch({
+		executablePath: chromePath,
+		headless: false,
+		args: ["--start-maximized"],
+	});
+
+	// const browser = await puppeteer.launch({ headless: false }); // headless: true para rodar em segundo plano
+	const page = await browser.newPage();
+
+	// for (const id of ids) {
+	// for (let a = 1; a < 2; a++) {
+	// const url = `https://conta.oxpay.com.br/contract/${ids[0]}/1`;
+	const url = `http://localhost:3000/contract/${ids[0]}/1`;
+	console.log(`Acessando: ${url}`);
+	await page.goto(url, { waitUntil: "networkidle2" });
+
+	// Adicionar um pequeno delay entre as requisições
+	// await page.waitForTimeout(20000);
+	// await page.evaluate(() => setTimeout(() => console.log("Tempo"), [5000]));
+	// await page
+	// 	.waitForSelector(
+	// 		"/html/body/pdf-viewer//viewer-toolbar//div/div[3]/viewer-download-controls//cr-icon-button"
+	// 	)
+	// 	.then((e) => console.log("First: ", e));
+	// }
+	// }
+	// page.mouse.move(200, 500);
+	await page.waitForSelector("#download");
+	await page.click("#download");
+
+	// await page.keyboard.type("Hello");
+
+	console.log("Processo concluído.");
+	// await browser.close();
+}
+
+const idsDemo = [
+	"BSZptCjvXtnIklDi9AGi3oCV8PHXydNv",
+	"N8ZjvXUAswOvhOThDxzFwByfLH8OMyJp",
+	"NHcmwGhtyMtHYuvj7QL1V8rdBmu3kHB6",
+];
+start(idsDemo);
